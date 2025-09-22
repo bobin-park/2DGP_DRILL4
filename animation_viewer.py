7cbec63119b11a549255fbb78410126c15b2cab9
Park bobin <asapdodin@tukorea.ac.kr>
2025-09-22 12:47:10 +0900

    제자리에서 동작 5번 반복

--- animation_viewer.py
+++ animation_viewer.py
@@ -2,13 +2,13 @@ from pico2d import *
 
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('images/Woodcutter_attack1.png')
-
+attackcharacter = load_image('images/Woodcutter_attack1.png')
+imageFrame=6
 frame=0
-for x in range (0,800,10):
+for x in range (0,imageFrame*6,1):
     clear_canvas()
     grass.draw(400,30)
-    character.clip_draw(frame * 48, 0, 48, 48, x, 300, 400, 400)
+    attackcharacter.clip_draw(frame * 48, 0, 48, 48, 400, 300, 450, 450)
     update_canvas()
     frame=(frame+1)%7
     delay(0.05)
