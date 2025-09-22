e5573cbf12d551ef60e0e4390e4dcbb3d5666f4b
Park bobin <asapdodin@tukorea.ac.kr>
2025-09-22 12:44:27 +0900

    첫번째 모션 동작

--- animation_viewer.py
+++ animation_viewer.py
@@ -2,9 +2,15 @@ from pico2d import *
 
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('Woodcutter_attack1.png')
+character = load_image('images/Woodcutter_attack1.png')
 
 frame=0
-
+for x in range (0,800,10):
+    clear_canvas()
+    grass.draw(400,30)
+    character.clip_draw(frame * 48, 0, 48, 48, x, 300, 400, 400)
+    update_canvas()
+    frame=(frame+1)%7
+    delay(0.05)
 
 close_canvas()
