d7ad875e270ba37c5e929735d26bbf344d4749d7
Park bobin <asapdodin@tukorea.ac.kr>
2025-09-22 12:56:35 +0900

    5번 동작 후 다음 동작 무한 반복 구현

--- animation_viewer.py
+++ animation_viewer.py
@@ -11,18 +11,27 @@ motionNum=1 #모션 애니 번호
 imageFrame=6 #모션 애니 프레임수
 frame=0
 if motionNum==1 or motionNum==4:
-    imageFrame=6
+    imageFrame=7
 elif motionNum==2:
-    imageFrame=3
-elif motionNum==3:
     imageFrame=4
+elif motionNum==3:
+    imageFrame=3
 while 1:
-    for x in range(0, imageFrame * 6, 1):
+    for x in range(0, imageFrame * 5, 1):
         clear_canvas()
         grass.draw(400, 30)
-        attackcharacter.clip_draw(frame * 48, 0, 48, 48, 400, 300, 450, 450)
+        if(motionNum==1):
+            attackcharacter.clip_draw(frame * 48, 0, 48, 48, 400, 300, 450, 450)
+        elif(motionNum==2):
+            hurtcharacter.clip_draw(frame * 48, 0, 48, 48, 400, 300, 450, 450)
+        elif (motionNum == 3):
+            idlecharacter.clip_draw(frame * 48, 0, 48, 48, 400, 300, 450, 450)
+        elif (motionNum == 4):
+            deathcharacter.clip_draw(frame * 48, 0, 48, 48, 400, 300, 450, 450)
         update_canvas()
-        frame = (frame + 1) % 7
+        frame = (frame + 1) % imageFrame
         delay(0.05)
-
-close_canvas()
+    if(motionNum>=4):
+        motionNum=1
+    else:
+        motionNum = motionNum+1
