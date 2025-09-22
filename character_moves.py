60e1158286de696a59eec6d97629225b715e9fa5
Park bobin <asapdodin@tukorea.ac.kr>
2025-09-22 12:27:26 +0900

    9.22.class

--- character_moves.py
+++ character_moves.py
@@ -3,9 +3,15 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('run_animation.png')
 
-# 여기를 채우세요.
+frame=0
+for x in range (0,800,10):
+    clear_canvas()
+    grass.draw(400,30)
+    character.clip_composite_draw(frame*100,0,100,100,0,'h',x,90,200,200)
+    update_canvas()
+    frame=(frame+1)%8
+    delay(0.05)
 
 close_canvas()
-
